3de25567516399d7a179a6e37d8dc89567777fce
Game-Dev2003 <gamekps832003@gmail.com>
2026-07-15 10:40:00 +0700

    fix: clean up next.config.mjs syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: ['192.168.1.4'],
     images: {
         unoptimized: true
     }
-}
+};
 
-export default nextConfig
+export default nextConfig;

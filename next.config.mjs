040e1523b87f05f52c2663ada2aee7018ace64d2
Game-Dev2003 <gamekps832003@gmail.com>
2026-07-15 11:29:29 +0700

    fix: clean up next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
     images: {
         unoptimized: true
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://gamedev-anika-backend.onrender.com/api/:path*', // ส่งผ่าน Vercel ไปหา Render
+            },
+        ];
+    },
 };
 
 export default nextConfig;
